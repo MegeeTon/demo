ada7fefbbd81d6a19016bccba1f2dae211ef7ff7
megee t <1611017032@qq.com>
2023-05-29 15:21:09 +0800

    [update] niuke

--- source/niuke.h
+++ source/niuke.h
@@ -112,7 +112,7 @@ public:
 
     //输入一个长度为 n 的链表，设链表中的元素的值为 ai ，返回该链表中倒数第k个节点。
     //如果该链表长度小于k，请返回一个长度为 0 的链表。
-    ListNode* FindKthToTail(ListNode* pHead, int k)
+    ListNode* FindKthToTail(ListNode* pHead, int k);
 };
 ////////////////////////////////////////////////////////////////////////////////
 // Functions
